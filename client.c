883c06d789489affe32d341b2ab9f11c53dc0b35
Masoud Sadeghi <35397912+pyDeb@users.noreply.github.com>
2020-08-21 17:48:42 -0400

    Update client.c

--- client.c
+++ client.c
@@ -1,173 +1,160 @@
-#include <fcntl.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/signal.h>
-#include <sys/wait.h>
-#include <netinet/in.h>
-#include <sys/un.h>
-#include <sys/socket.h>
-#include<arpa/inet.h>
-#include <sys/types.h>
-#include <dirent.h>
+#include <arpa/inet.h>
+#include <stdbool.h>
+
+#define SIZE 1024
 
 #define BUFF_SIZE 1024
 
-int main(int argc, char *argv[])
+
+
+bool inputValidation(char *input)
 {
 
-    char message[BUFF_SIZE], user_input[BUFF_SIZE], end_of_file[2] = {0x4, 0x0};
-    int server, portNumber, pid, n;
+    if((strncmp(input, "get", 3) == 0) || (strncmp(input, "put", 3) == 0) || (strncmp(input, "quit", 4) == 0))
+        return true;
 
-    // server socket address
-    struct sockaddr_in servAdd;
-
-    // argument validation
-    if(argc != 3)
-    {
-        printf("Call model: %s <IP Address> <Port Number>\n", argv[0]);
-        exit(0);
-    }
-
-    // create socket
-    if ((server = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("Cannot create socket: ");
-        exit(1);
-    }
-
-    // configure server socket address parameter
-    servAdd.sin_family = AF_INET;
-    sscanf(argv[2], "%d", &portNumber);
-    servAdd.sin_port = htons((uint16_t)portNumber);
-
-    // changing the presesntation of IP
-    if(inet_pton(AF_INET, argv[1], &servAdd.sin_addr) < 0)
-    {
-        perror("inet_pton() failed to assign the address");
-        exit(2);
-    }
-
-    // establishing a connection with server
-    if(connect(server, (struct sockaddr *) &servAdd, sizeof(servAdd))<0)
-    {
-        perror("Sorry mate, I couldn't connect");
-        exit(3);
-    }
-
-    // reading server's message initially
-    read(server, message, BUFF_SIZE);
-    message[strlen(message)] = '\0';
-    printf("\n\nMessage received from server: \n%s"), message;
-
-
-    while (1)
-    {
-        char *filename;
-        char dummy[1];
-        dummy[0] = 0;
-        write(server, dummy, 1);
-        memset(user_input, '\0', BUFF_SIZE);
-        fgets(user_input, BUFF_SIZE, stdin);
-        user_input[strlen(user_input) - 1] = '\0';
-        printf("%s\n", user_input);
-        write(server, user_input, strlen(user_input) + 1);
-        
-        //wait for server's response to see if the user's input is successfully evaluated
-        read(server, message, BUFF_SIZE);
-        message[strlen(message)] = '\0';
-        if(message[0] != '0')
-        {
-            printf("Please enter 'get filename', 'put filename', or 'quit'\n");
-            continue;
-        }
-
-        
-        if(user_input[0] == 'q' && user_input[1] == 'u' && user_input[2] == 'i' && user_input[3] == 't')
-        {
-            printf("We hope you enjoyed using our awesome client-server application! :)\n");
-            close(server);
-            return 0;
-        }
-
-        filename = user_input + 4;
-
-        if(user_input[0] == 'g' && user_input[1] == 'e' && user_input[2] == 't')
-        {
-            int fd;
-            // ask the server if the user's requested file is a valid one
-            ssize_t len = send(server, filename, strlen(filename), 0);
-            
-            read(server, message, BUFF_SIZE);
-            message[strlen(message)] = '\0';
-        
-            if(message[0] != '0')
-            {
-                printf("Err: File was not found on server!\n");
-                continue;
-            }
-
-            printf("The file you requested was found on the server.\n");
-            filename[len] = '\0';
-            printf("FILENAME: %s\n" , filename);
-            fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
-            memset(message, '\0', BUFF_SIZE);
-            //sleep(3);
-            //send(server, message, 1, 0);
-            len = read(server, message, BUFF_SIZE);
-            // printf("message: %s\n", message);
-            // printf("%d\n", len);
-            message[len] = '\0';
-            while(message[0] != 0x4)
-            {
-                printf("FILE CONTENT: %s\n", message);
-                fflush(stdout);
-                write(fd, message, strlen(message));
-                len = read(server, message, BUFF_SIZE);
-                message[len] = '\0';
-            }
-            //sleep(5);
-            write(server, dummy, 1);
-            sleep(4);
-            read(server, message, BUFF_SIZE + 1);
-            message[strlen(message)] = '\0';
-            printf("Message from server:\n%s\n", message);
-            fflush(stdout);
-            write(fd, end_of_file, 1);
-            close(fd);
-            continue;
-        }
-
-        // if(user_input[0] == 'p' && user_input[1] == 'u' && user_input[2] == 't')
-        // {
-        //     int fd;
-        //     char buff[BUFF_SIZE];
-
-        //     if((fd = open(filename, O_RDONLY)) == -1)
-        //     {
-        //         perror("Error While Opening File: ");
-        //         continue;
-        //     }
-            
-
-        //     while((read(fd, buff, BUFF_SIZE)) > 0)
-        //     {
-        //         buff[strlen(buff)] = '\0'; 
-        //         send(server, buff, strlen(buff), 0);
-        //     }
-
-        //     send(server, end_of_file, 1, 0);
-        //     close(fd);
-        //     //finish the stream for now
-        //}
-        
-    }
+    return false;
 
 }
 
 
 
+void putFile(FILE *fp, int sockfd){
+  int n;
+  char data[SIZE] = {0};
+  char end_of_file[2] = {0x4, 0x0};
 
+  while(fgets(data, SIZE, fp) != NULL) {
+      printf("SENT %s\n", data);
+    if (send(sockfd, data, sizeof(data), 0) == -1) {
+      perror("[-]Error in sending file.");
+      exit(1);
+    }
+    bzero(data, SIZE);
+  }
+    send(sockfd, end_of_file, 2, 0);
+    return;
+  
+}
+
+void getFile(int sockfd, char *filename){
+  int n;
+  FILE *fp;
+  //char *filename = "recv.txt";
+  char buffer[SIZE];
+
+  fp = fopen(filename, "wb+");
+  while (1) {
+    n = recv(sockfd, buffer, SIZE, 0);
+    if(buffer[0] == 0x4)
+        return;
+    
+    if(buffer[0] == 0x0)
+        return;
+    printf("RECEIVED: %s\n", buffer);
+    if (n <= 0){
+      break;
+      return;
+    }
+    fprintf(fp, "%s\n", buffer);
+    
+    bzero(buffer, SIZE);
+  }
+  fclose(fp);
+  return;
+}
+
+
+int main()
+{
+  char *ip = "137.207.82.52";
+  int port = 1212;
+  int e;
+
+  int sockfd;
+  struct sockaddr_in server_addr;
+  FILE *fp;
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0) {
+    perror("[-]Error in socket");
+    exit(1);
+  }
+  printf("[+]Server socket created successfully.\n");
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons((uint16_t)port);
+  
+    if(inet_pton(AF_INET, ip, &server_addr.sin_addr) < 0)
+    {
+        perror("inet_pton() failed to assign the address");
+        exit(2);
+    }
+
+    if(connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr))<0)
+    {
+        perror("Sorry mate, I couldn't connect");
+        exit(3);
+    }
+
+	printf("[+]Connected to Server.\n");
+
+
+    while(19)
+    {
+        char *filename, user_input[BUFF_SIZE];
+        memset(user_input, '\0', BUFF_SIZE);
+        fgets(user_input, BUFF_SIZE, stdin);
+        user_input[strlen(user_input) - 1] = '\0';
+        // printf("%s\n", user_input);
+        write(sockfd, user_input, strlen(user_input) + 1);
+        
+        if((strncmp(user_input, "quit", 4)) == 0)
+        {
+            printf("We hope you enjoyed using our awesome client-server application! :)\n");
+            close(sockfd);
+            return 0;
+        }
+
+        filename = user_input + 4;
+
+        if((strncmp(user_input, "put", 3)) == 0)
+        {
+            filename = user_input + 4;
+            fp = fopen(filename, "r");
+            if (fp == NULL) {
+                perror("[-]Error in reading file.");
+                exit(1);
+            }       
+
+            putFile(fp, sockfd);
+            fclose(fp);
+
+        }
+
+        if((strncmp(user_input, "get", 3)) == 0)
+        {
+            filename = user_input + 4;
+            getFile(sockfd, filename);
+
+        }
+
+    }
+//   fp = fopen(filename, "r");
+//   if (fp == NULL) {
+//     perror("[-]Error in reading file.");
+//     exit(1);
+//   }
+
+//   send_file(fp, sockfd);
+//   printf("[+]File data sent successfully.\n");
+
+// 	printf("[+]Closing the connection.\n");
+//   close(sockfd);
+
+  return 0;
+}
